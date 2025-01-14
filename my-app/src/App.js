9c63d4310124f8213e2aa0e5725034813b10b36f
Ayush0007-rx <kumarayush03554@gmail.com>
2025-01-14 09:52:13 +0530

    Internship: Remove extra stuffs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Header from './Header';
 import './App.css';
 import exampleImage from './image1.jpg';
+import exampleimage from './image3.jpg';
+import exampleimage4 from './image4.jpg';
+import exampleimage5 from './image5.jpg';
+
 
 function App() {
   return (
@@ -18,16 +22,29 @@ function App() {
       <h2 style={{ marginLeft: '250px' }}>Who's Using Unstop..?</h2>
       </div>
       <div className="boxes-container">
-        <div className="box">
-          <strong>Students and Professionals</strong>
-          <p>hii</p>
+       <div className="row" style={{ border: '1px solid black' , display: 'flex', flexDirection: 'row', alignItems: 'centre', paddingLeft: '10px', paddingRight: '10px' }}>
+          <div className="box" style={{ paddingRight: '10px' }}>
+            <strong>Students and Professionals</strong>
+            <p>Unlock Your Potential: Compete,Build Resume,Grow and get Hired!</p>
+          </div>
+          <div>
+          <img src={exampleimage} alt="Example" style={imageStyle1} />
+          </div>
         </div>
+        <div className="row" style={{ border: '1px solid black' , display: 'flex', flexDirection: 'row', alignItems: 'centre', paddingLeft: '10px', paddingRight: '10px' }}>
         <div className="box">
           <strong>Company and Recruters</strong>
+          <p>Discover RIght Talent: Hire,Engage and Brand Like Never Before!</p>
         </div>
+        <img src={exampleimage4} alt="Example" style={imageStyle2} />
+        </div>
+        <div className="row" style={{ border: '1px solid black' , display: 'flex', flexDirection: 'row', alignItems: 'centre', paddingLeft: '10px', paddingRight: '10px' }}>
         <div className="box">
           <strong>Colleges</strong>
+          <p>Bridge Academia and Industry: Empower STudents with Real-World Opportunities!</p>
         </div>
+        <img src={exampleimage5} alt="Example" style={imageStyle3} />
+      </div>
       </div>
     </div>
   );
@@ -38,5 +55,21 @@ const imageStyle = {
   height: 'auto',
   marginTop: '20px' // Optional: Add some margin to separate the image from the header
 };
+
+const imageStyle1 = {
+  width: '70px',
+  height: '60px' // Optional: Add some margin to separate the image from the header
+};
+
+const imageStyle2 = {
+  width: '70px',
+  height: '60px' // Optional: Add some margin to separate the image from the header
+};
+
+const imageStyle3 = {
+  width: '70px',
+  height: '60px' // Optional: Add some margin to separate the image from the header
+};
+
 
 export default App;
